1d97edb843f7802eadb73b9bd3be4aa2f14959cd
chernenkoik <chernenkoik@tutu.tech>
2024-06-14 23:56:33 -0300

    change endpoint config for production

--- config/runtime.exs
+++ config/runtime.exs
@@ -55,7 +55,7 @@ if config_env() == :prod do
   config :plata, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :plata, PlataWeb.Endpoint,
-    url: [host: host, port: 80, scheme: "http"],
+    url: [host: host, port: 443, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
